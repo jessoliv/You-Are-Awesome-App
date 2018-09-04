eb508d1e88f81d8886a6cb5066019c354ca6037c
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2018-09-03 23:43:10 -0400

    added in arrays

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet weak var messageLabel: UILabel!
-	
+	var index = 0
 	// Code below executes when the app's view first loads
 
 	override func viewDidLoad() {
@@ -20,19 +20,37 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func showMessagePressed(_ sender: UIButton) {
-		let message1 = "You Are Awesome!"
-		let message2 = "You Are Great!"
-		let message3 = "You Are Amazing!"
+		let messages = ["You Are Awesome!",
+						"You Are Great!",
+						"You Are Fantastic!",
+						"when the Genius bar needs help, they call you",
+						"You Brighten My Day!",
+						"You make me smile!",
+						"You are da bomb!",
+						"Hey, Fabulous",
+						"You Are Tremendous!",
+						"I can't wait to download your app"]
 		
-		
-		if messageLabel.text == message1 {
-			messageLabel.text = message2
-		} else if messageLabel.text == message2 {
-			messageLabel.text = message3
-		} else {
-			messageLabel.text = message1
+		messageLabel.text = messages[index]
+		if index == messages.count - 1 {
+			index = 0
+		}else{
+			index = index + 1
 		}
-	
+		
+//		let message1 = "You Are Awesome!"
+//		let message2 = "You Are Great!"
+//		let message3 = "You Are Amazing!"
+//
+//
+//		if messageLabel.text == message1 {
+//			messageLabel.text = message2
+//		} else if messageLabel.text == message2 {
+//			messageLabel.text = message3
+//		} else {
+//			messageLabel.text = message1
+//		}
+
 		
 		
 	}
