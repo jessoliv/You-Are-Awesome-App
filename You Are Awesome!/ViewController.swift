9b2aba99a1a1c0a719b527ff16fba519a7c83328
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2018-09-03 21:04:58 -0400

    added two buttons - "You Are Awesome!" and "You Are Great!"

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet weak var messageLabel: UILabel!
+	
+	// Code below executes when the app's view first loads
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
-
+	@IBAction func showOtherMessage(_ sender: Any) {
+		messageLabel.text = "You Are Great!"
+	}
+	
+	@IBAction func showMessagePressed(_ sender: UIButton) {
+		messageLabel.text = "You Are Awesome!"
+		
+		
+	}
 }
 
